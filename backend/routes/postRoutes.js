876a8d8ee11ddbe5b9a5d3e308a51b91d0b07648
Nabin bhandari <bnabin555@gmail.com>
2024-03-29 11:36:38 +0545

    post controller added

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { getFeedPosts , getUserPosts, likePost}  = require("../controllers/postController");
-const {verifyToken } = require("../middlewares/protectMiddleware");
+const verifyToken  = require("../middlewares/protectMiddleware");
 
 
 const router = express.Router();
 
-// router.get("/", verifyToken, getFeedPosts);
-// router.get("/:userId/posts",verifyToken,getUserPosts);
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts",verifyToken,getUserPosts);
 
-// router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/like", verifyToken, likePost);
 
 
 module.exports = router;
